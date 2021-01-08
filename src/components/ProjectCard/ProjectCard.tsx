9ced01209f99d31f96b7edc3a399bffa8250843c
Cole Gannaway <43042842+cole-gannaway@users.noreply.github.com>
2021-01-08 12:25:01 -0500

    Fixed Projects resizing of components

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -4,20 +4,14 @@ import React, { Component } from 'react';
 
 class ProjectCard extends Component<{ title: string, description: string, icon: JSX.Element }, {}> {
     public render() {
-        return <div>
+        return <div >
             <Grid container spacing={3}>
-                <Grid item xs={1}>
-                    {/* Padding */}
-                </Grid>
-                <Grid item xs={2}>
+                <Grid item style={{ width: '20%', maxWidth: '100px' }}>
                     {this.props.icon}
                 </Grid>
-                <Grid item xs={8}>
+                <Grid item style={{ width: '80%' }}>
                     <div><b>{this.props.title}</b></div>
                     <p>{this.props.description}</p>
-                </Grid>
-                <Grid item xs={1}>
-                    {/* Padding */}
                 </Grid>
             </Grid>
         </div>;
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -8,28 +8,41 @@ import FlashOnIcon from '@material-ui/icons/FlashOn';
 import WavesIcon from '@material-ui/icons/Waves';
 
 import ProjectsData from './ProjectsCardData.json'
+import { Grid } from '@material-ui/core';
 
 function Projects() {
     const projectsData = ProjectsData;
     const getIconByTitle = (title: string) => {
         switch (title) {
             case "Sudoku Solver":
-                return <AppsIcon style={{ width: '100%', maxWidth: 100, height: '100%', maxHeight: 100 }}></AppsIcon>
+                return <AppsIcon style={{ width: '100%', height: '100%' }}></AppsIcon>
             case "Financial Planner":
-                return <TrendingUpIcon style={{ width: '100%', maxWidth: 100, height: '100%', maxHeight: 100 }}></TrendingUpIcon>
+                return <TrendingUpIcon style={{ width: '100%', height: '100%' }}></TrendingUpIcon>
             case "Course Analysis":
-                return <SportsGolfIcon style={{ width: '100%', maxWidth: 100, height: '100%', maxHeight: 100 }}></SportsGolfIcon>
+                return <SportsGolfIcon style={{ width: '100%', height: '100%' }}></SportsGolfIcon>
             case "Fast Feed":
-                return <FlashOnIcon style={{ width: '100%', maxWidth: 100, height: '100%', maxHeight: 100 }}></FlashOnIcon>
+                return <FlashOnIcon style={{ width: '100%', height: '100%' }}></FlashOnIcon>
             case "Up And Down The River":
-                return <WavesIcon style={{ width: '100%', maxWidth: 100, height: '100%', maxHeight: 100 }}></WavesIcon>
+                return <WavesIcon style={{ width: '100%', height: '100%' }}></WavesIcon>
             default:
-                return <AppsIcon style={{ width: '100%', maxWidth: 100, height: '100%', maxHeight: 100 }}></AppsIcon>
+                return <AppsIcon style={{ width: '100%', height: '100%' }}></AppsIcon>
         }
     };
     const projectCards = projectsData.projects.map((data, i) => {
         const icon = getIconByTitle(data.title);
-        return (<ProjectCard key={'card' + i} title={data.title} description={data.description} icon={icon}></ProjectCard>);
+        return (
+            <Grid key={'card' + i} container spacing={3}>
+                <Grid item xs={1}>
+                    {/* Padding */}
+                </Grid>
+                <Grid item xs={10}>
+                    <ProjectCard title={data.title} description={data.description} icon={icon}></ProjectCard>
+                </Grid>
+                <Grid item xs={1}>
+                    {/* Padding */}
+                </Grid>
+            </Grid>
+        );
     })
     return (
         <div>
